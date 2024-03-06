c1a6d381cc14b24df50211c75e7359a5167f639e
Vitor Campos <vitormdc@hotmail.com>
2024-03-06 15:34:32 -0300

    fix: layout and key event on ios

--- src/app/shared/components/list-itens/list-itens.component.ts
+++ src/app/shared/components/list-itens/list-itens.component.ts
@@ -1,22 +1,24 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Item } from '../../../core/interfaces/responses/commom-response.interface';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { merge, startWith, switchMap, of } from 'rxjs';
 
 @Component({
   selector: 'app-list-itens',
   templateUrl: './list-itens.component.html',
-  styleUrl: './list-itens.component.scss'
+  styleUrl: './list-itens.component.scss',
 })
-export class ListItensComponent implements OnInit{
-
+export class ListItensComponent implements OnInit {
   length = 0;
   pageSize = 5;
   pageIndex = 0;
-  pageSizeOptions = [5, 10, 25];
+  pageSizeOptions = [5];
 
-  pageEvent: PageEvent = new PageEvent;
+  dataSource: Item[] = [];
 
-
+  pageEvent: PageEvent = new PageEvent();
+  @ViewChild(MatPaginator, { static: true })
+  paginator!: MatPaginator;
 
   @Input() listItens: Item[] = [];
   @Input() title: string = '';
@@ -27,8 +29,8 @@ export class ListItensComponent implements OnInit{
 
   loadItens() {
     this.length = this.listItens.length;
+    this.linkListToPaginator();
   }
-
 
   handlePageEvent(e: PageEvent) {
     this.pageEvent = e;
@@ -39,7 +41,26 @@ export class ListItensComponent implements OnInit{
 
   setPageSizeOptions(setPageSizeOptionsInput: string) {
     if (setPageSizeOptionsInput) {
-      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+      this.pageSizeOptions = setPageSizeOptionsInput
+        .split(',')
+        .map((str) => +str);
     }
+  }
+
+  linkListToPaginator() {
+    // merge simply joins all this operators and creates an       //observable that listen to paginator page events
+    merge(this.paginator.page)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          // creates an obserbable of sample data
+          return of(this.listItens); // Replace 'data' with 'this.listItens' or define 'data' variable
+        })
+      )
+      .subscribe((res) => {
+        const from = this.paginator.pageIndex * 5;
+        const to = from + 5;
+        this.dataSource = res.slice(from, to);
+      });
   }
 }
